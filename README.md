a378e8f087f06738c51093c6680ecea193548d75
DimiDR <dima_rybak@yahoo.de>
2023-08-06 13:30:32 +0200

    changes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PS1Screenshots
 Power Shell Script to make screenshots every 10 secconds
+
+# Usage
+
+- Change the path where you want to save the pictures
+- Change number of repetitions and sleep time in seconds
+If you want to run the program for 2 hours and make a screenshot every 10 seconds, set the counter to 10 * 6 * 2 = 120
+- Open CMD and run:
+powershell.exe -windowstyle hidden -file ...\Screenshots\screenshot.ps1
+
+Change the path, to the path of you program.
---
+++ screenshot.ps1
@@ -0,0 +1,25 @@
+$path = ".../Screenshots/Images/" # change the path where the screenshots are saved
+$fileName = "Test"
+ $i = 0
+ for ($i=0; $i -le 6){ #change number of repetitions (6=1Min for 10sec sleep)
+    $date = Get-Date -Format yyyyMMdd-hhmm
+    $file = $path + $i +".bmp" 
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+    # Gather Screen resolution information
+    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $Width = $Screen.Width
+    $Height = $Screen.Height
+    $Left = $Screen.Left
+    $Top = $Screen.Top
+    # Create bitmap using the top-left and bottom-right bounds
+    $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+    # Create Graphics object
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    # Capture screen
+    $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+    # Save to file
+    $bitmap.Save($File)
+    sleep 1 #change number of secconds
+    $i++
+ }
